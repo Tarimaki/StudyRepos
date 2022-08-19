35ed9025f679fe9fc14ceda5f01dc94ea77bdd6f
tarimaki <ryu.1152.toaru.taka@gmail.com>
2022-08-19 12:17:02 +0900

    カラム個数チェック Exception追加

--- csv読み取り/ColumnCount/count.php
+++ csv読み取り/ColumnCount/count.php
@@ -1,13 +1,32 @@
 <?php
-    $csv = fopen('./test.csv', 'r');
-    $i = 0;
+    class ColumnCountException extends Exception{
+        public function __construct(){
+            parent::__construct("カラム個数間違ってるよ",1);
+        }
+    }
 
-    while($row = fgetcsv($csv)){
-        //配列の中身のカウントを数えることでカラム個数を抽出
-        $count = count($row);
+    
 
-        echo "<p>ROW[" . $i . "] = " . $count . "</p>";
-
-        $i++;
+    function ColumnCount(){
+        $csv = fopen('./test.csv', 'r');
+        $i = 0;
+        while($row = fgetcsv($csv)){
+            //配列の中身のカウントを数えることでカラム個数を抽出
+            $count = count($row);
+    
+            echo "<p>ROW[" . $i . "] = " . $count . "</p>";
+    
+            $i++;
+    
+            if($count != 5){
+                throw new ColumnCountException();
+            }
+        }
+    }
+    
+    try{
+        ColumnCount();
+    }catch(Exception $ex){
+        echo "<p>" . $ex . "</p>";
     }
 ?>
